2962284dc62e7f82bb41e023ef7e1b6fd429f276
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2025-12-18 12:40:17 +0700

    feat: remove BillingUsage component and its integration from billing settings

--- app/(protected)/settings/_components/billing.tsx
+++ app/(protected)/settings/_components/billing.tsx
@@ -1,5 +1,4 @@
 import BillingPlan from "./billing-plan";
-import BillingUsage from "./billing-usage";
 import BillingPayment from "./billing-payment";
 import BillingHistory from "./billing-history";
 
@@ -7,7 +6,6 @@ export default function BillingPage() {
   return (
     <div className="space-y-6 md:space-y-4">
       <BillingPlan />
-      <BillingUsage />
       <BillingPayment />
       <BillingHistory />
     </div>
