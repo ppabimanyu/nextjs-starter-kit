9a9914fa333375e5a40e0a0e4afdf5fc4d359924
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2025-12-19 16:42:28 +0700

    feat: Use environment variable for axios base URL instead of hardcoded localhost.

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { env } from "./env";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: env.NEXT_PUBLIC_SITE_URL,
   headers: {
     "Content-Type": "application/json",
   },
